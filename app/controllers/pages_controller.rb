792c6904307c18719d718a86820e0df6c7f86b77
victorhebdo <victor@gabella.org>
2018-11-05 18:09:02 +0000

    lesson 5 rails Mikart

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+
+  def salut
+    @name = params[:name]
+  end
+
+  def home
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  get '/salut/(:name)', to: 'pages#salut'
+  resources :posts
+  resources :categories
+
 end
